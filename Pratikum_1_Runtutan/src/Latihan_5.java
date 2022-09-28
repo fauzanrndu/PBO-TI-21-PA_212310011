84c4acfc7a94614fbb513070e9235692396fdd8b
Asus <Asus@DESKTOP-M1QMF08>
2022-09-28 11:31:05 +0700

    added class Latihan_5.java

---
+++ Pratikum_1_Runtutan/src/Latihan_5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Latihan_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x;
+		System.out.print("Masukan Kalimat : ");
+		Scanner input = new Scanner(System.in);
+		x = input.nextLine();
+		System.out.println(x.toUpperCase());
+	}
+
+}
