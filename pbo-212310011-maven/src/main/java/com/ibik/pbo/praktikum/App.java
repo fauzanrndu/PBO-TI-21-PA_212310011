cb6aa1735bd9ecb7817dc9b63a18d8fb2586b1ec
fauzanrndu <113745758+fauzanrndu@users.noreply.github.com>
2023-01-09 09:08:18 +0700

    Add files via upload

---
+++ pbo-212310011-maven/src/main/java/com/ibik/pbo/praktikum/App.java
@@ -0,0 +1,13 @@
+package com.ibik.pbo.praktikum;
+import javax.swing.JOptionPane;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       JOptionPane.showMessageDialog(null, "hello world");
+    }
+}
